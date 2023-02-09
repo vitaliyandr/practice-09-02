b6341de6fec2035969b3075d6c0334803f20ff93
vitaliyandr <116282877+vitaliyandr@users.noreply.github.com>
2023-02-09 16:42:07 +0200

    Add files via upload

---
+++ ex0/ex1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello, World!" << endl;
+
+    cout << "\\\\Denis Ritchi\\\\" << endl;
+
+}
---
+++ ex0/ex2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "To be, or not to be: that is the question: " << endl;
+
+    cout << "Whether 'tis nobler in the mind to suffer " << endl;
+
+    cout << "The slings and arrows of outrageous fortune, " << endl;
+
+    cout << "Or to take arms against a sea of troubles, " << endl;
+
+    cout << "And by opposing end them? " << endl;
+
+    cout << "       William Shakespeare" << endl;
+
+}
+
